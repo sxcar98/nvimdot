965df5f57507e6c5fceaabcb6f60b6d3399cfd18
sxcar98 <sxcar98@gmail.com>
2025-06-24 11:03:16 +0800

    use client instead.

--- init.lua
+++ init.lua
@@ -1,25 +1,29 @@
+--supprese warning on lack of perl and ruby
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Load default configurations and plugins
 for _, source in ipairs({
-  "plugins",
-  "options",
-  "keymaps",
-  "autocmds",
+	"plugins",
+	"options",
+	"keymaps",
+	"autocmds",
 }) do
-  local ok, fault = pcall(require, source)
-  if not ok then
-    vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
-  end
+	local ok, fault = pcall(require, source)
+	if not ok then
+		vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
+	end
 end
--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -2,38 +2,38 @@ local null_ls = require("null-ls")
 local bltns = null_ls.builtins
 
 local sources = {
-	bltns.formatting.stylua,
-	require("none-ls.formatting.rustfmt"),
-	bltns.formatting.prettier,
-	bltns.formatting.blackd,
-	bltns.formatting.isort,
-	bltns.formatting.clang_format.with({
-		filetypes = { "c", "cpp", "cc", "h", "hpp" },
-	}),
+  bltns.formatting.stylua,
+  require("none-ls.formatting.rustfmt"),
+  bltns.formatting.prettier,
+  bltns.formatting.blackd,
+  bltns.formatting.isort,
+  bltns.formatting.clang_format.with({
+    filetypes = { "c", "cpp", "cc", "h", "hpp" },
+  }),
 }
 
 local options = {
 
-	sources = sources,
+  sources = sources,
 
-	-- format on save
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({
-				group = vim.api.nvim_create_augroup("LspFormatting", {}),
-				buffer = bufnr,
-			})
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = vim.api.nvim_create_augroup("LspFormatting", {}),
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({
-						async = false,
-					})
-				end,
-			})
-		end
-	end,
+  -- format on save
+  on_attach = function(client, bufnr)
+    if client:supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = vim.api.nvim_create_augroup("LspFormatting", {}),
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = vim.api.nvim_create_augroup("LspFormatting", {}),
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({
+            async = false,
+          })
+        end,
+      })
+    end
+  end,
 }
 
 null_ls.setup(options)
